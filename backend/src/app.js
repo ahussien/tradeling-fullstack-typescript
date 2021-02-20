bb7f6383bc32919b72c943d5ad433dd1e5ad7f45
Ahmed Hussien <ahussien.gomaa@gmail.com>
2021-02-20 20:54:42 +0400

    Cleaning

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var logger = require('morgan');
 var cors = require('cors')
 var swaggerUi = require('swagger-ui-express');
-var swaggerDocument = require('./swagger.json');
+var swaggerDocument = require('../swagger.json');
 
 var apiRouter = require('./routes/api');
 
@@ -11,8 +11,6 @@ app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-
 app.use('/api', apiRouter);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
